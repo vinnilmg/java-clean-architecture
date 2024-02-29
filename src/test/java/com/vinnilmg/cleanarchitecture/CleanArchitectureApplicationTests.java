6392e0db7d1088577ce63b9996ac250a491daee4
vinnilmg <vini.delimagomes@gmail.com>
2024-02-28 21:18:50 -0300

    init project

---
+++ src/test/java/com/vinnilmg/cleanarchitecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinnilmg.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
